ae972810224b2b7b3ac69ab6b5e1b35cb4408f89
Matthew Gaddes <matthew.gaddes@gmail.com>
2020-10-01 16:28:48 -0700

    Add HowWeCanHelp section

---
+++ src/components/HowWeCanHelp/Card.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Img from 'gatsby-image';
+
+import { Card as BaseCard } from '../common';
+
+const Div = styled.div`
+  display: grid;
+  grid-template:
+    '1fr 1fr';
+`;
+
+const Title = styled.div`
+  font-size: 1.2rem;
+`;
+
+const Line = styled.div`
+  width: 60px;
+  height: 3px;
+  background-color: var(--dusty-blue);
+  margin: 12px 0;
+`;
+
+const ImgWrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  
+  margin: 0 0 0 1rem;
+`;
+
+const Card = props => {
+  return (
+    <BaseCard>
+      <Div>
+        <div>
+          <Title>{props.title}</Title>
+          <Line />
+          {props.children}
+        </div>
+        <ImgWrapper>
+          <Img
+            fixed={props.img}
+            alt={props.alt}
+          />
+        </ImgWrapper>
+      </Div>
+    </BaseCard>
+  );
+};
+
+const { shape, arrayOf, oneOfType, string } = PropTypes;
+
+Card.propTypes = {
+  children: oneOfType([shape({}), arrayOf(shape({}))]).isRequired,
+  img: shape({}).isRequired,
+  title: string,
+  alt: string,
+};
+
+Card.defaultProps = {
+  title: '',
+  alt: '',
+};
+
+export default Card;
--- src/components/HowWeCanHelp/HowWeCanHelp.js
+++ src/components/HowWeCanHelp/HowWeCanHelp.js
@@ -1,16 +1,91 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { graphql, useStaticQuery } from 'gatsby';
 
-const HowWeCanHelp = props => {
+import { Section } from '../common';
+import Card from './Card';
+
+const Div = styled.div`  
+  display: grid;
+  grid-template:
+    '1fr 1fr'
+    '1fr 1fr'
+    '1fr 1fr';
+  grid-gap: 2rem;
+`;
+
+const Em = styled.i`
+  margin: 0;
+`;
+
+
+const HowWeCanHelp = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "images/mugshot.jpg" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          fixed(width: 200, height: 150) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    <div>
-      helloooooo
-    </div>
+    <Section title="How we can help your business" bgColor="dark">
+      <Div>
+        <Card
+          title="Analytics"
+          img={data.image.childImageSharp.fixed}
+          alt="Woman drawing a graph on a whiteboard"
+        >
+          <div>With added Google Analytics you'll know <Em>exactly</Em> how many new customers you're attracting.</div>
+        </Card>
+
+        <Card
+          title="Growth"
+          img={data.image.childImageSharp.fixed}
+          alt="Woman standing next to a bar chart"
+        >
+          <div>Your new site is the perfect platform on which to build your business. Add an online store to sell faster than ever before.</div>
+        </Card>
+
+        <Card
+          title="Branding"
+          img={data.image.childImageSharp.fixed}
+          alt="Man stood next to a logo"
+        >
+          <div>Logo design and a fresh color palette can rejuvenate your brand and create new business opportunities.</div>
+        </Card>
+
+        <Card
+          title="Discoverability"
+          img={data.image.childImageSharp.fixed}
+          alt="An office with a location marker above"
+        >
+          <div>Leave your customers with no doubt as to who you are, what you do, and where they can find you.</div>
+        </Card>
+
+        <Card
+          title="Enhanced presence"
+          img={data.image.childImageSharp.fixed}
+          alt="A computer screen with content flowing in"
+        >
+          <div>Add key business information such as maps, menus and product catalogues.</div>
+        </Card>
+
+        <Card
+          title="Personal touch"
+          img={data.image.childImageSharp.fixed}
+          alt="A woman holding a heart-shaped balloon"
+        >
+          <div>We work with you every step of the way to ensure your new site solves your most pressing business needs.</div>
+        </Card>
+      </Div>
+    </Section>
   );
-};
-
-HowWeCanHelp.propTypes = {
-
 };
 
 export default HowWeCanHelp;
