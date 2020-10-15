800e881ac4c306709638206e341f6a94e759b011
Matthew Gaddes <matthew.gaddes@gmail.com>
2020-10-14 17:39:33 -0700

    Use png rather than svg in nav

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -1,43 +1,30 @@
 import React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
-// TODO: delete me!
 import Image from 'gatsby-image';
 import scrollTo from 'gatsby-plugin-smoothscroll';
 import styled from 'styled-components';
 
-import Logo from '../../assets/logo_text.svg';
-
 const NavStyles = styled.nav`
   background-color: var(--light-gray);
   border-bottom: 3px solid var(--dark-blue);
+  
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
 
-  ul {
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: space-between;
-    align-items: center;
-
-    height: 70px;
-    margin: 0;
-    padding: 0.5rem 2rem;
-    list-style: none;
-  }
-`;
-
-const Span = styled.span`
-  svg {
-    width: 150px;
-  }
+  height: 70px;
+  margin: 0;
+  padding: 0.5rem 2rem;
 `;
 
 const Nav = () => {
-  // TODO: delete me!
   const data = useStaticQuery(graphql`
     query {
       image: file(relativePath: {eq: "logo_text.png"}) {
         childImageSharp {
-          fluid(maxWidth: 1200) {
-            ...GatsbyImageSharpFluid_tracedSVG
+          fixed(width: 155, quality: 100) {
+            ...GatsbyImageSharpFixed_withWebp_tracedSVG
           }
         }
       }
@@ -46,26 +33,13 @@ const Nav = () => {
 
   return (
     <NavStyles>
-      <ul>
-        <li>
-          <Span>
-            <Link to="/">
-              <Logo alt="Arlo Design logo" />
-            </Link>
-
-            {/* TODO: delete me! AND logo_text.png */}
-            {/*<Link to="/">*/}
-            {/*  <Image*/}
-            {/*    fluid={data.image.childImageSharp.fluid}*/}
-            {/*    alt="Arlo Design logo"*/}
-            {/*  />*/}
-            {/*</Link>*/}
-          </Span>
-        </li>
-        <li>
-          <button onClick={() => scrollTo('#contact')}>Contact</button>
-        </li>
-      </ul>
+      <Link to="/">
+        <Image
+          fixed={data.image.childImageSharp.fixed}
+          alt="Arlo Design logo"
+        />
+      </Link>
+      <button onClick={() => scrollTo('#contact')}>Contact</button>
     </NavStyles>
   );
 };
