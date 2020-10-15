86b5444305ab5f6cfdad56f834699f807b343127
Matthew Gaddes <matthew.gaddes@gmail.com>
2020-10-15 08:41:44 -0700

    Update text across site

--- src/components/HowDoesItWork/HowDoesItWork.js
+++ src/components/HowDoesItWork/HowDoesItWork.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import scrollTo from 'gatsby-plugin-smoothscroll';
 
 import { Section } from '../common';
 import Card from './Card';
@@ -34,7 +35,24 @@ const Masonry = styled.div({
     columns: '1',
     maxWidth: 650,
   },
+
+  '.list': {
+      margin: '0.5rem 0 0 2rem',
+
+    '> li': {
+      margin: '0.5rem 0',
+    },
+  },
 });
+
+const Scroll = styled.span`
+  cursor: pointer;
+  text-decoration: underline;
+  
+  :hover {
+    text-decoration: none;
+  }
+`;
 
 const HowDoesItWork = () => (
   <Section title="How does it work?">
@@ -46,10 +64,13 @@ const HowDoesItWork = () => (
           Img={Consult}
           alt="Two people communicating using screens"
         >
-          <div>Enter your details in our contact form and we’ll call you back for a free initial consultation.</div>
-          <div>We’ll use this time to get to know each other better, discuss your business goals and design ideas, and establish a scope and budget for the project.</div>
+          <div>
+            Enter your details in our&nbsp;
+            <Scroll onClick={() => scrollTo('#contact')}>contact form</Scroll>
+            &nbsp;and we’ll call you back for a free initial consultation.
+          </div>
+          <div>We’ll use this time to get to know each other better, discuss your business goals and design ideas, and establish a scope and budget for your project.</div>
           <div>Following our consultation you’ll receive a summary of our notes. We’ll ask you to review these to confirm our project goals are aligned before detailed design begins.</div>
-          <div>* may be worth creating a basic questionnaire to ensure nothing is missed? We can pre-fill answers based on the initial chat as needed.</div>
         </Card>
 
         <Card
@@ -59,14 +80,14 @@ const HowDoesItWork = () => (
           alt="A man standing in front of a bar chart"
         >
           <div>Key topics that will help us to define the project scope:</div>
-          <div>Business goals - Where do you see your business in the next 12 months? What currently brings in the most revenue, and where would you like us to focus our efforts?</div>
-          <div>Branding - Would you like to use your existing branding? Do you want to build a brand from scratch?</div>
-          <div>Budget - What’s your budget for this project? We will openly discuss our rates to ensure they align with your expectations.</div>
-          <div>Timeframe - Do you have a timeframe in mind?</div>
-          <div>Together we’ll review everything we’ve discussed to ensure our visions for your project are aligned. We’ll then issue you with a summary, scope and brief contractual documents to review.</div>
-          <div>This will be the final step before detailed design begins!</div>
-          <div>* Before design begins, we ask for a down payment of 50% (project based on lump sum)</div>
-          <div>* We ask for the first 3 hours up front (project based on hourly rate)</div>
+          <ul className="list">
+            <li><b>Business goals</b> - Where do you see your business in the next 12 months? What currently brings in the most revenue, and where would you like to focus our efforts?</li>
+            <li><b>Branding</b> - Would you like to use your existing branding? Do you want to build a brand from scratch?</li>
+            <li><b>Budget</b> - What’s your budget for this project? We'll openly discuss our rates to ensure they align with your expectations.</li>
+            <li><b>Timeframe</b> - Do you have a timeframe in mind?</li>
+          </ul>
+          <div>Together we’ll review everything we’ve discussed to ensure our visions for your project are aligned. We’ll then issue you with a summary, scope and brief contractual documents.</div>
+          <div>This is the final step before detailed design begins!</div>
         </Card>
 
         <Card
@@ -76,10 +97,10 @@ const HowDoesItWork = () => (
           alt="Two people placing sticky notes on a board"
         >
           <div>During this stage we’ll design the layout, colors and imagery for your site. There will be two main stages:</div>
-          <div>A) Initial design - layout, wireframes, placeholders for images, color palettes, brand logo (if requested), font options.</div>
-          <div>* basic wireframe in Figma</div>
-          <div>B) Detailed design - images, layout for both desktop and mobile, fonts, colors, clickable prototype.</div>
-          <div>* Figma clickable prototype</div>
+          <ol className="list">
+            <li><b>Initial design</b> - We'll sketch a wireframe layout to position your content on the page. We design responsively to ensure your site looks great on all size devices.</li>
+            <li><b>Detailed design</b> - Your imagery and brand colors will be added. We'll issue a prototype for your review before diving in to development.</li>
+          </ol>
         </Card>
 
         <Card
@@ -91,7 +112,7 @@ const HowDoesItWork = () => (
           <div>The design is done and now it’s time to build your site!</div>
           <div>We use the latest web technologies to ensure your site is lightning fast and responsive across all devices.</div>
           <div>SEO is baked in for maximum discoverability on the most popular search engines; you’ll never be more than a click away!</div>
-          <div>At the end of this stage we’ll issue a fully-functional site for your review at our unique domain. Upon receiving final payment we flip the switch, transfer to your domain of choice and hand over your site.</div>
+          <div>At the end of this stage we’ll issue a fully-functional site for your review at our unique domain. After confirming that you love the new design, we transfer to your domain of choice and hand over your site.</div>
         </Card>
 
         <Card
