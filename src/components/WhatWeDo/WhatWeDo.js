bacd7435c80cb66fd1fafbf154668607d346b312
Matthew Gaddes <matthew.gaddes@gmail.com>
2020-10-16 15:51:23 -0700

    Rewrite createCards function

--- src/components/WhatWeDo/WhatWeDo.js
+++ src/components/WhatWeDo/WhatWeDo.js
@@ -37,49 +37,61 @@ const Flex = styled.div`
   }
 `;
 
-// TODO: create 'createCard' function that accepts plain data and returns Card
-const cards = [
-  <Card
-    index={0}
-    title="Design"
-    Img={Design}
-    alt="Designers hard at work"
-  >
-    <div>Attract new customers and increase sales with a unique design created just for your business</div>
-    <div>We ensure your visitors can access key information and purchase your products with ease; streamlining workflows across your site and squashing pain points along the way.</div>
-  </Card>,
-
-  <Card
-    index={1}
-    title="Development"
-    Img={Dev}
-    alt="A programmer with six screens"
-  >
-    <div>Modern techniques and best practices are utilized to create a fast and efficient business portal. Say goodbye to long loading times, large images and spinning loaders that never seem to end!</div>
-    <div>New visitors will find your information in record time, and existing customers will be delighted with your new, best-in-class performance.</div>
-  </Card>,
-
-  <Card
-    index={2}
-    title="Branding"
-    Img={Brand}
-    alt="A man choosing a colour palette"
-  >
-    <div>Strengthen your online presence with a thorough branding review; services include logo creation and colour scheme design.</div>
-    <div>We enhance your existing brand and for new companies (or those looking to start from scratch) we build from the ground up, involving you every step of the way.</div>
-  </Card>,
-
-  <Card
-    index={3}
-    title="SEO"
-    Img={Seo}
-    alt="A woman with a giant magnifying glass"
-  >
-    <div>What use is your brand new site if your customers can’t find it?</div>
-    <div>Search Engine Optimization helps to maximize your online presence and ensure that users can find you on search engines like Google.</div>
-    <div>Static pages, server-side rendering and comprehensive metadata are just some of the techniques we employ to maximize discoverability for your business.</div>
-  </Card>,
+const cardData = [
+  {
+    title: 'Design',
+    image: Design,
+    alt: 'Designers hard at work',
+    content: [
+      'Attract new customers and increase sales with a unique design created just for your business',
+      'We ensure your visitors can access key information and purchase your products with ease; streamlining workflows across your site and squashing pain points along the way.',
+    ],
+  },
+  {
+    title: 'Development',
+    image: Dev,
+    alt: 'A programmer with six screens',
+    content: [
+      'Modern techniques and best practices are utilized to create a fast and efficient business portal. Say goodbye to long loading times, large images and spinning loaders that never seem to end!',
+      'New visitors will find your information in record time, and existing customers will be delighted with your new, best-in-class performance.',
+    ],
+  },
+  {
+    title: 'Branding',
+    image: Brand,
+    alt: 'A man choosing a colour palette',
+    content: [
+      'Strengthen your online presence with a thorough branding review; services include logo creation and colour scheme design.',
+      'We enhance your existing brand and for new companies (or those looking to start from scratch) we build from the ground up, involving you every step of the way.',
+    ],
+  },
+  {
+    title: 'SEO',
+    image: Seo,
+    alt: 'A woman with a giant magnifying glass',
+    content: [
+      'What use is your brand new site if your customers can’t find it?',
+      'Search Engine Optimization helps to maximize your online presence and ensure that users can find you on search engines like Google.',
+      'Static pages, server-side rendering and comprehensive metadata are just some of the techniques we employ to maximize discoverability for your business.',
+    ],
+  },
 ];
+
+// This is a function rather than <Cards /> component because <Carousel />
+//  requires individual children to work, rather than one <Cards />
+//  component which contains all the individual cards.
+const createCards = data => (
+  data.map(card => (
+    <Card
+      key={card.title}
+      title={card.title}
+      Img={card.image}
+      alt={card.alt}
+    >
+      {card.content.map(item => <div>{item}</div>)}
+    </Card>
+  ))
+);
 
 const WhatWeDo = () => {
   const isMediumScreen = useMediaQuery(mediaQueries.mediumWidth);
@@ -89,12 +101,12 @@ const WhatWeDo = () => {
       {isMediumScreen
         ? <Flex>
             <Carousel>
-              {cards.map(card => card)}
+              {createCards(cardData)}
             </Carousel>
           </Flex>
 
         : <Grid>
-            {cards.map(card => card)}
+            {createCards(cardData)}
           </Grid>
       }
     </Section>
